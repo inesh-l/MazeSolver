06b8b19fa18fedbf551e635fd2c2c6c1e7e9e156
inesh-l <56318873+inesh-l@users.noreply.github.com>
2022-09-07 22:41:42 -0400

    Create helper methods

--- main.py
+++ main.py
@@ -24,3 +24,50 @@ class Pixel:
         self.index = None
 
 
+def bubble_up(queue, index):
+    if index <= 0:
+        return queue
+    p_index = (index - 1) // 2
+    if queue[index].d < queue[p_index].d:
+        queue[index], queue[p_index] = queue[p_index], queue[index]
+        queue[index].index_in_queue = index
+        queue[p_index].index_in_queue = p_index
+        queue = bubble_up(queue, p_index)
+    return queue
+
+
+def bubble_down(queue, index):
+    length = len(queue)
+    lc_index = 2 * index + 1
+    rc_index = lc_index + 1
+    if lc_index >= length:
+        return queue
+    if lc_index < length <= rc_index:  # just left child
+        if queue[index].d > queue[lc_index].d:
+            queue[index], queue[lc_index] = queue[lc_index], queue[index]
+            queue[index].index_in_queue = index
+            queue[lc_index].index_in_queue = lc_index
+            queue = bubble_down(queue, lc_index)
+    else:
+        small = lc_index
+        if queue[lc_index].d > queue[rc_index].d:
+            small = rc_index
+        if queue[small].d < queue[index].d:
+            queue[index], queue[small] = queue[small], queue[index]
+            queue[index].index_in_queue = index
+            queue[small].index_in_queue = small
+            queue = bubble_down(queue, small)
+    return queue
+
+
+def find_path(maze_img, begin, end):
+    priority_queue = []
+    pixel_matrix = np.full((maze_img.shape[0], maze_img.shape[1]), None)
+
+    for r in range(maze_img.shape[0]):
+        for c in range(maze_img.shape[1]):
+            pixel_matrix[r][c] = Pixel(c, r)
+            pixel_matrix[r][c].index = len(priority_queue)
+            priority_queue.append(pixel_matrix[r][c])
+    pixel_matrix[begin[0]][begin[1]].d = 0
+    priority_queue = bubble_up(priority_queue, pixel_matrix[begin[0]][begin[1]].index)
