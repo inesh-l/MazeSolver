2f186eb39270349c9f3819fdc5930bfea7a909d2
inesh-l <56318873+inesh-l@users.noreply.github.com>
2022-11-03 18:11:05 -0400

    file upload

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
+import os
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = '/'
-
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         f = request.files['file']
-        f.save(f.filename)
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
+        print(os.path.splitext('static/uploads/'+f.filename))
 
     return render_template('index.html')
 
